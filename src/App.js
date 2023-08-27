94f2a3c5838f42ee83b56e283b0376047b0a8b85
Tran Cong Luc <luc.tran@hellohealthgroup.com>
2023-08-27 10:03:40 +0700

    update version to demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import 'antd/dist/antd.css'
 import '@luctc/template-builder/dist/index.css';
 import 'codemirror/lib/codemirror.css'
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button, Drawer, Space } from 'antd';
 import { ArrowDownOutlined, MobileOutlined } from '@ant-design/icons';
-import { TemplateBuilder, templateBuilderToHTML, download } from '@luctc/template-builder';
+import { TemplateBuilder, templateBuilderToHTML, download, templateConfig } from '@luctc/template-builder';
 import { StyledApp, StyledHead, StyledBuilderWrap, StyledPreviewIframe } from './App.styled';
+import ErrorBoundary from './ErrorBoundary'
+
+templateConfig.templateLocale = 'vi-VN';
+    templateConfig.listVariables = [
+      { key: 'email', label: 'Email' }
+    ]
+templateConfig.formatVariable = (key) => `{{var:${key}}}`
 
 function App() {
   const ref = useRef({
@@ -15,6 +22,73 @@ function App() {
   const [htmlTemplate, setHtmlTemplate] = useState('');
   const [isMobile, setIsMobile] = useState(false);
 
+  useEffect(() => {
+    templateConfig.onCopilot(({ key, currentEditor }) => {
+      alert(key)
+    });
+  }, []);
+
+  useEffect(() => {
+    templateConfig.setFileHandler((resolve, reject) => {
+      // alert('Handle upload from external app');
+      const inputFile = document.createElement('input');
+      inputFile.type = 'file';
+      inputFile.accept = 'image/png, image/gif, image/jpeg';
+      inputFile.addEventListener('change', handleFileUpload);
+
+      function handleFileUpload(event) {
+        const target = event.target;
+        const file = (target.files && target.files[0]) || null;
+
+        if (file && file.type.startsWith('image/')) {
+          resolve({
+            url: URL.createObjectURL(file),
+            context: '',
+          });
+        }
+      }
+
+      inputFile.click();
+    });
+  }, []);
+
+  useEffect(() => {
+    templateConfig.setFetchArticle(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          // resolve(null);
+          resolve({
+            url: 'https://example.com',
+            title: 'Đau hạ sườn trái là bệnh gì? Không chỉ là vấn đề tiêu hóa',
+            thumbnail: 'https://picsum.photos/300',
+            description:
+              'Đau hạ sườn trái là cơn đau chủ yếu ở góc nửa phía dưới bên trái của bụng. Khu vực vùng bụng bên trái có thể mềm khi chạm vào kèm theo cơn đau dữ dội và khiến toàn bộ vùng bụng bị căng cứng. Nguyên nhân gây đau chủ yếu liên quan đến đường tiêu hóa, nhưng cũng có thể liên quan đến các cơ quan khác, chẳng hạn như da, mạch máu, đường tiết niệu hoặc cơ quan sinh sản ở phụ nữ và nam giới.',
+            authorName: 'Trúc Phạm',
+            authorAvatar: 'https://picsum.photos/150/150',
+          });
+        }, 500);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    templateConfig.setFetchProduct(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({
+            url: 'https://example.com',
+            title: "Viên Uống Mọc Tóc Puritan's Pride Biotin 10000 mcg - 100 viên",
+            thumbnail: 'https://picsum.photos/400',
+            priceFinal: 505000,
+            priceRegular: 600000,
+            discountPercent: 16,
+            buttonText: 'Mua Ngay',
+          });
+        }, 500);
+      });
+    });
+  }, []);
+  
   return (
     <StyledApp>
       <StyledHead>
@@ -31,7 +105,9 @@ function App() {
         </Button>
       </StyledHead>
       <StyledBuilderWrap>
-        <TemplateBuilder localStorageKey="builderData" ref={ref} />
+        <ErrorBoundary>
+          <TemplateBuilder localStorageKey="builderData" ref={ref} templateType="email" />
+        </ErrorBoundary>
       </StyledBuilderWrap>
       <Drawer
         title="Preview" //
